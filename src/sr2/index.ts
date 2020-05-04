5603e2f28e90ea97da237a6e9c2646ba496d3e5e
Hendrik Mans <hendrik@mans.de>
2020-05-04 18:46:37 +0200

    Preload assets!

--- src/sr2/index.ts
+++ src/sr2/index.ts
@@ -1,5 +1,11 @@
 import { Entity } from "@bigby/core";
-import { Renderer, Ticker, Transform, Sprite } from "@bigby/game";
+import {
+  Renderer,
+  Ticker,
+  Transform,
+  Sprite,
+  ResourceLoader,
+} from "@bigby/game";
 
 /* Set up PIXI */
 import * as PIXI from "pixi.js";
@@ -8,7 +14,11 @@ PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 PIXI.settings.STRICT_TEXTURE_CACHE = true;
 
 /* Main scene */
-const mainScene = new Entity({ name: "Main Scene", behaviors: [Transform] });
+const mainScene = new Entity({
+  name: "Main Scene",
+  behaviors: [Transform, ResourceLoader],
+});
+
 mainScene.addChild({
   name: "Player",
   behaviors: [Transform, [Sprite, { uri: "/assets/lemming.png" }]],
@@ -18,7 +28,12 @@ mainScene.addChild({
 const game = new Entity({
   name: "Main Game",
   behaviors: [Renderer, Ticker],
-  children: [mainScene],
 });
 
 game.awake();
+
+/* Preload main scene resources */
+mainScene.preload();
+mainScene.getBehavior(ResourceLoader)?.loader.load(() => {
+  game.addChild(mainScene);
+});
