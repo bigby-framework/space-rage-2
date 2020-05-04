2ec66a46911bdfdc07aafcc7f8b56714e50213bd
Hendrik Mans <hendrik@mans.de>
2020-05-04 20:16:08 +0200

    player -> Player

--- src/sr2/scenes/main/index.ts
+++ src/sr2/scenes/main/index.ts
@@ -1,10 +1,10 @@
 import { Entity } from "@bigby/core";
 import { ResourceLoader, Transform } from "@bigby/game";
-import player from "./player";
+import Player from "./Player";
 
 export default () =>
   new Entity({
     name: "Main Scene",
     behaviors: [Transform, ResourceLoader],
-    children: [player()],
+    children: [Player()],
   });
