576bd9a4d90f56031c8017a482767829e4ffc5cb
Hendrik Mans <hendrik@mans.de>
2020-05-04 18:35:01 +0200

    New "game"

--- src/sr2/index.ts
+++ src/sr2/index.ts
@@ -1,13 +1,24 @@
 import { Entity } from "@bigby/core";
-import { Renderer, Ticker } from "@bigby/game";
+import { Renderer, Ticker, Transform, Sprite } from "@bigby/game";
 
 /* Set up PIXI */
 import * as PIXI from "pixi.js";
 window.PIXI = PIXI;
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+PIXI.settings.STRICT_TEXTURE_CACHE = true;
+
+/* Main scene */
+const mainScene = new Entity({ name: "Main Scene", behaviors: [Transform] });
+mainScene.addChild({
+  name: "Player",
+  behaviors: [Transform, [Sprite, { uri: "/assets/lemming.png" }]],
+});
 
 /* Game */
-
-const game = new Entity({ name: "Main Game", behaviors: [Renderer, Ticker] });
+const game = new Entity({
+  name: "Main Game",
+  behaviors: [Renderer, Ticker],
+  children: [mainScene],
+});
 
 game.awake();
