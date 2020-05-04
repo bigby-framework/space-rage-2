b0d52e4cc3e56b4f0cfa95c0517593a02ccc75be
Hendrik Mans <hendrik@mans.de>
2020-05-04 16:45:22 +0200

    Yup

--- src/sr2/index.ts
+++ src/sr2/index.ts
@@ -1,4 +1,13 @@
+import { Entity } from "@bigby/core";
+import { Renderer, Ticker } from "@bigby/game";
+
 /* Set up PIXI */
 import * as PIXI from "pixi.js";
 window.PIXI = PIXI;
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
+/* Game */
+
+const game = new Entity({ name: "Main Game", behaviors: [Renderer, Ticker] });
+
+game.awake();
