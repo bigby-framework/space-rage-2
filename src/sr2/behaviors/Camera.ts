ce5187a16c4b356a7d70c60efc2ef0ec58e08151
Hendrik Mans <hendrik@mans.de>
2020-05-05 13:06:22 +0200

    Add glMatrix

--- src/sr2/behaviors/Camera.ts
+++ src/sr2/behaviors/Camera.ts
@@ -1,21 +1,32 @@
-import { GameBehavior, Transform } from "@bigby/game";
+import { GameBehavior, Transform, Renderer } from "@bigby/game";
+import { vec2 } from "gl-matrix";
 
 export default class Camera extends GameBehavior {
   target?: Transform;
 
+  private renderer?: Renderer;
+  private offset?: vec2;
+
+  awake() {
+    this.renderer = this.getNearestBehavior(Renderer);
+    this.offset = vec2.fromValues(
+      this.renderer!.width / 2,
+      this.renderer!.height / 2
+    );
+  }
+
   lateUpdate() {
-    if (!this.target || !this.transform) return;
+    if (!this.target || !this.transform || !this.renderer) return;
 
     /* Look at target */
-    const position = {
-      x: -this.target.position.x,
-      y: -this.target?.position.y,
-    };
+    const position = vec2.fromValues(
+      -this.target.position.x,
+      -this.target.position.y
+    );
 
     /* Apply offset */
-    position.x += 200;
-    position.y += 200;
+    vec2.add(position, position, this.offset!);
 
-    this.transform.position = position;
+    this.transform.position.set(...position);
   }
 }
