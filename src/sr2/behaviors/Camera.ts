512298294898aaee2bfd15dc13aba0b7c8fdf4f6
Hendrik Mans <hendrik@mans.de>
2020-05-05 12:55:12 +0200

    Cleanup

--- src/sr2/behaviors/Camera.ts
+++ src/sr2/behaviors/Camera.ts
@@ -4,11 +4,18 @@ export default class Camera extends GameBehavior {
   target?: Transform;
 
   lateUpdate() {
-    if (this.target) {
-      this.transform?.position.set(
-        -this.target.position.x,
-        -this.target.position.y
-      );
-    }
+    if (!this.target || !this.transform) return;
+
+    /* Look at target */
+    const position = {
+      x: -this.target.position.x,
+      y: -this.target?.position.y,
+    };
+
+    /* Apply offset */
+    position.x += 200;
+    position.y += 200;
+
+    this.transform.position = position;
   }
 }
