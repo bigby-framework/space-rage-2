1f21efe304f8ebc42b4dfac468261afe2bbeb878
Hendrik Mans <hendrik@mans.de>
2020-05-08 19:11:52 +0200

    Add RB2D to asteroid

--- src/sr2/scenes/main/Asteroid.ts
+++ src/sr2/scenes/main/Asteroid.ts
@@ -1,5 +1,6 @@
 import { Entity } from "@bigby/core";
 import { Sprite, Transform } from "@bigby/game";
+import { RigidBody2D } from "@bigby/physics2d";
 
 export default () =>
   new Entity({
@@ -7,5 +8,6 @@ export default () =>
     behaviors: [
       [Transform, { position: { x: 300, y: 300 } }],
       [Sprite, { resource: "assets/sprites/asteroid.png", anchor: 0.5 }],
+      [RigidBody2D],
     ],
   });
