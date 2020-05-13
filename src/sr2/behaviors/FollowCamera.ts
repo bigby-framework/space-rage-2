12ef6ddf186207d11e4fe848a31bed62b8114bf6
Hendrik Mans <hendrik@mans.de>
2020-05-13 16:26:34 +0200

    Remove glMatrix

--- src/sr2/behaviors/FollowCamera.ts
+++ src/sr2/behaviors/FollowCamera.ts
@@ -1,16 +1,15 @@
-import { GameBehavior, Transform, Renderer } from "@bigby/game";
-import { vec2 } from "gl-matrix";
+import { GameBehavior, Transform, Renderer, vec2 } from "@bigby/game";
 import { $up } from "@bigby/core";
 
 export default class FollowCamera extends GameBehavior {
   target?: Transform;
 
   private renderer?: Renderer;
-  private offset?: vec2;
+  private offset?: vec2.IVec2;
 
   awake() {
     this.renderer = $up(this, Renderer);
-    this.offset = vec2.fromValues(
+    this.offset = vec2.make(
       this.renderer!.width / 2,
       this.renderer!.height / 2
     );
@@ -20,14 +19,14 @@ export default class FollowCamera extends GameBehavior {
     if (!this.target || !this.transform || !this.renderer) return;
 
     /* Look at target */
-    const position = vec2.fromValues(
+    const position = vec2.make(
       -this.target.position.x,
       -this.target.position.y
     );
 
     /* Apply offset */
-    vec2.add(position, position, this.offset!);
+    const pos = vec2.add(position, this.offset!);
 
-    this.transform.position.set(...position);
+    this.transform.position.set(pos.x, pos.y);
   }
 }
