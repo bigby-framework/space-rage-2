5c2b140cf8de0f6acd828bad30fe27adb2f994df
Hendrik Mans <hendrik@mans.de>
2020-05-13 12:50:17 +0200

    Import bullet physics from PE

--- src/sr2/scenes/main/Bullet.ts
+++ src/sr2/scenes/main/Bullet.ts
@@ -1,7 +1,8 @@
 import { Entity } from "@bigby/core";
 import { Sprite, Transform } from "@bigby/game";
+import { PhysicsDataLoader2D, RigidBody2D } from "@bigby/physics2d";
 import { AutoDestroy } from "@bigby/timers";
-import { RigidBody2D, CircleCollider2D } from "@bigby/physics2d";
+import { bullet } from "../../../../assets/physics.json";
 import BulletController from "./behaviors/BulletController";
 
 export default ({ position = { x: 0, y: 0 }, rotation = 0 } = {}) => {
@@ -11,7 +12,7 @@ export default ({ position = { x: 0, y: 0 }, rotation = 0 } = {}) => {
       [Transform, { position, rotation, scale: 0.5 }],
       [Sprite, { resource: "assets/sprites/bullet.png", anchor: 0.5 }],
       [RigidBody2D, { fixedRotation: true, bullet: true }],
-      [CircleCollider2D, { radius: 0.2 }],
+      [PhysicsDataLoader2D, { data: bullet }],
       [AutoDestroy, { duration: 1 }],
       [BulletController],
     ],
