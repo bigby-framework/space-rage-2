969d302077dd87ec637cba233c077b9d3710d43d
Hendrik Mans <hendrik@mans.de>
2020-05-13 16:30:40 +0200

    Prevent PIXI from saying hello

--- src/sr2/index.ts
+++ src/sr2/index.ts
@@ -14,6 +14,7 @@ import PlayerInput from "./scenes/main/behaviors/PlayerInput";
 
 /* Set up PIXI */
 window.PIXI = PIXI;
+PIXI.utils.skipHello();
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 PIXI.settings.STRICT_TEXTURE_CACHE = true;
 
